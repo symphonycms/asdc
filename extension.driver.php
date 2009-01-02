36ee7f8d7c7930112e8003ed67b94499b8c3e533
Alistair Kearney <alistair@pointybeard.com>
2009-01-02 14:03:00 +1100

    Initial Commit

---
+++ extension.driver.php
@@ -0,0 +1,16 @@
+<?php
+
+	Class extension_ASDC extends Extension{
+
+		public function about(){
+			return array('name' => 'Advanced Symphony Database Connector (ASDC)',
+						 'version' => '1.0',
+						 'release-date' => '2009-01-02',
+						 'author' => array('name' => 'Alistair Kearney',
+										   'website' => 'http://pointybeard.com',
+										   'email' => 'alistair@pointybeard.com')
+				 		);
+		}
+		
+	}
+		

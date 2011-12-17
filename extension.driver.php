6965813281f8e7690f660ed24b6c824cccd536cb
Brendan Abbott <brendan@bloodbone.ws>
2011-12-17 13:25:29 +1000

    Add extension.meta.xml. Deprecated extension. Bump to 1.4

--- extension.driver.php
+++ extension.driver.php
@@ -3,14 +3,16 @@
 	Class extension_ASDC extends Extension{
 
 		public function about(){
-			return array('name' => 'Advanced Symphony Database Connector (ASDC)',
-						 'version' => '1.3',
-						 'release-date' => '2010-02-12',
-						 'author' => array('name' => 'Alistair Kearney',
-										   'website' => 'http://pointybeard.com',
-										   'email' => 'alistair@pointybeard.com')
-				 		);
+			return array(
+				'name' => 'Advanced Symphony Database Connector (ASDC)',
+				'version' => '1.4',
+				'release-date' => '2010-07-22',
+				'author' => array(
+					'name' => 'Alistair Kearney',
+					'website' => 'http://pointybeard.com',
+					'email' => 'alistair@pointybeard.com')
+				);
 		}
-		
+
 	}
-		
+
